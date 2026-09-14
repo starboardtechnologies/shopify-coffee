8deda7ad239c98a8b611a75759cbea11056be160
MacBook Pro <macbookpro@MacBookdeMacBook-Pro.local>
2026-09-15 01:10:51 +0800

    Remove Vercel configuration for Oxygen deployment

--- project-one/react-router.config.ts
+++ project-one/react-router.config.ts
@@ -1,12 +1,7 @@
 // react-router.config.ts
 
 import type {Config} from '@react-router/dev/config';
-import {vercelPreset} from '@vercel/react-router/vite';
 
 export default {
   ssr: true,
-
-  presets: [
-    vercelPreset(),
-  ],
 } satisfies Config;
